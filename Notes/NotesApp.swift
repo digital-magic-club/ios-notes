d7e1401cb5291288826c8ab4de077beaac530ac3
Guillaume Bellut <guillaume@bellut.com>
2020-10-03 22:50:54 +0200

    First commit

---
+++ Notes/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes
+//
+//  Created by Guillaume Bellut on 03/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NotesView()
+        }
+    }
+}
---
+++ Notes/NotesView.swift
@@ -0,0 +1,20 @@
+//
+//  NotesView.swift
+//  Notes
+//
+//  Created by Guillaume Bellut on 03/10/2020.
+//
+
+import SwiftUI
+
+struct NotesView: View {
+    var body: some View {
+        Text("Hello, world!")
+    }
+}
+
+struct NotesView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotesView()
+    }
+}
